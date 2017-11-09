f7af11e7f5b463f4cafd72fb54b82c0baa659a6d
Sascha Niemann <sascha.niemann@r132-10-w10.SCHULE-ISERV>
2017-11-09 14:41:21 +0100

    hazards

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,7 +3,9 @@ using System.Collections;
 
 public class GameController : MonoBehaviour
 {
-	public GameObject hazard;
+	public GameObject hazard1;
+	public GameObject hazard2;
+	public GameObject hazard3;
 	public Vector3 spawnValues;
 	public int hazardCount;
 	public float spawnWait;
@@ -56,7 +58,10 @@ public class GameController : MonoBehaviour
 			{
 				Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
 				Quaternion spawnRotation = Quaternion.identity;
-				Instantiate (hazard, spawnPosition, spawnRotation);
+				int j = Random.Range (0, 3);
+				if(j==0)Instantiate (hazard1, spawnPosition, spawnRotation);
+				if(j==1)Instantiate (hazard2, spawnPosition, spawnRotation);
+				if(j==2)Instantiate (hazard3, spawnPosition, spawnRotation);
 				yield return new WaitForSeconds (spawnWait);
 			}
 			yield return new WaitForSeconds (waveWait);
